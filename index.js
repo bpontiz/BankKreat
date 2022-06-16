06b1f942feaa1a147212b81adcc1265736947d7b
brunopontiz <bpontiz@gmail.com>
2022-06-16 16:06:36 -0300

    Applying Functional Programming to the project: high order functions.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //* Consigna: CREAR UN SIMULADOR INTERACTIVO INCORPORANDO ARRAYS
 //*           Y UTILIZANDO METODOS Y CLASES VISTOS EN CLASE.
+//*           PRIMERA PRE ENTREGA DE TRABAJO FINAL
 
 //? Enunciado: Calcular la tasa de interés compuesto a una 
 //?            determinada cantidad de tiempo en meses.
 
-class Interes {
+class ServicioInteres {
   constructor(nombre, apellido, profesion) {
     this.nombre = nombre.toUpperCase();
     this.apellido = apellido.toUpperCase();
@@ -14,7 +15,7 @@ class Interes {
     /*
     * Descripción: función que muestra por consola los datos del usuario.
     */
-    console.log("Las credenciales del usuario son: "+"\n"+this.nombre+" "+this.apellido+"\n"+this.profesion);
+    console.log("Las credenciales del usuario son: "+this.profesion+" "+this.nombre+" "+this.apellido);
   }
   interesCompuesto() {
     /*
@@ -27,26 +28,41 @@ class Interes {
   tasaDeInteres = parseInt(prompt("Ingrese tasa de interés. Ej: 37%"));
   tiempoDeAhorro = parseInt(prompt("Ingrese tiempo de ahorro en meses. Ej: 2"));
   alert("Chequea la consola!");
-  for( let i = 0; i <= tiempoDeAhorro; i++ ){
+  console.log("El capital inicial fue de: " + capitalInicial + " $");
+  for( let i = 1; i <= tiempoDeAhorro; i++ ){
       //! Formula de interes compuesto
       capitalFinal = capitalInicial * ( 1 + ((tasaDeInteres/100)/12) ) ** i;
       let resultadoInteres = capitalFinal.toFixed(2);
-      console.log("El capital final al mes " + i + " es: " + resultadoInteres);  
+      console.log("El capital final al mes " + i + " es: " + resultadoInteres + " $");  
   }
   let diferenciaCapital = capitalFinal - capitalInicial;
   let resultadoCapital = diferenciaCapital.toFixed(2);
-  console.log("La ganancia total luego del tiempo de ahorro es de: " + resultadoCapital);
+  console.log("La ganancia total luego del tiempo de ahorro es de: " + resultadoCapital + " $");
   }
 }
 
-//* Declarando objetos para armado de base de datos.
-const database = [];
+//* Declaración de objetos para armado de base de datos.
+let databases = [];
 alert("Bienvenido a la calculadora de interés compuesto!");
 let pedirNombre = prompt("Ingrese su nombre:");
 let pedirApellido = prompt("Ingrese su apellido:");
-let pedirProfesion = prompt("Ingrese su profesion:");
-database.push(new Interes(pedirNombre, pedirApellido, pedirProfesion));
-for(const interes of database){
+let pedirProfesion = prompt("Ingrese su profesión:");
+databases.push(new ServicioInteres(pedirNombre, pedirApellido, pedirProfesion));
+for(const interes of databases){
   interes.mostrarUser();
   interes.interesCompuesto();
 }
+
+
+//* Búsqueda del usuario segun las credenciales introducidas 
+//* por él mismo. 
+function isInDataBase(testDatabase, askProfession) {
+  console.log("------------------"+"\n"+"[Testing] Validación de profesión."+"\n");
+  testDatabase.some(function(CompProfession){
+    // CompProfession se instancia con ServicioInteres, que es un objeto. //
+    economistValidation = askProfession === CompProfession.profesion;
+    console.log(economistValidation);
+  });
+}
+isInDataBase(databases, 'ECONOMISTA');
+
