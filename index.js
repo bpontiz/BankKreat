b01aaf61d9981feeb86f76d0d96e527f5d840d24
brunopontiz <bpontiz@gmail.com>
2022-06-23 15:15:12 -0300

    Applying event listeners on the project.

--- index.js
+++ index.js
@@ -17,36 +17,60 @@ class ServicioInteres {
     */
     console.log("Las credenciales del usuario son: "+this.profesion+" "+this.nombre+" "+this.apellido);
   }
-  interesCompuesto() {
-    /*
-    * Descripción: función que calcula el interés compuesto a partir
-    * de capital inicial sin reinversión parcial.
-    */
-  alert("A continuación ingreserá los datos de la calculadora.");
-  let capitalInicial = parseInt(prompt("Ingrese capital inicial. Ej: 3000"));
-  let tasaDeInteres = parseInt(prompt("Ingrese tasa de interés. Ej: 37%"));
-  let tiempoDeAhorro = parseInt(prompt("Ingrese tiempo de ahorro en meses. Ej: 2"));
-  let capitalReinversion = parseInt(prompt("Ingrese monto de reinversión mensual. Ej: 3000"));
-  alert("Chequea la consola!");
-  let capitalFinal = 0;
-  console.log("El capital inicial fue de: " + capitalInicial + " $");
-  console.log("El monto de reinversión fue de: " + capitalReinversion + " $");
-  for( let i = 1; i <= tiempoDeAhorro; i++ ){
+  
+}
+
+//* AddEventListener de cambios en los parametros introducidos por el usuario.
+document.getElementById("input_CI").addEventListener('change', interesCompuesto);
+document.getElementById("input_Interes").addEventListener('change', interesCompuesto);
+document.getElementById("input_Plazo").addEventListener('change', interesCompuesto);
+document.getElementById("input_CR").addEventListener('change', interesCompuesto);
+
+function interesCompuesto() {
+  /*
+  * Descripción: función que calcula el interés compuesto a partir
+  * de capital inicial con reinversión mensual.
+  */
+  let eventCapitalInicial = parseInt(document.getElementById("input_CI").value);
+  let eventTasaDeInteres = parseInt(document.getElementById("input_Interes").value);
+  let eventTiempoDeAhorro = parseInt(document.getElementById("input_Plazo").value);
+  let eventCapitalReinversion = parseInt(document.getElementById("input_CR").value);
+  if( eventCapitalInicial && eventTasaDeInteres && eventTiempoDeAhorro && eventCapitalReinversion ){
+    let capitalFinal = 0;
+    let resultadoInteres;
+    console.log(`El capital inicial fue de: ${eventCapitalInicial} $`);
+    console.log(`La tasa de interés anual fue de: ${eventTasaDeInteres} %`);
+    console.log(`El tiempo de ahorro fue de: ${eventTiempoDeAhorro} Meses`);
+    console.log(`El monto de reinversión fue de: ${eventCapitalReinversion} $`);
+    for( let i = 1; i <= eventTiempoDeAhorro; i++ ){
       //! Formula de interes compuesto
-      let resultadoInteres;
       if( i == 1 ){
-        capitalFinal = (capitalInicial) * ( 1 + ( (tasaDeInteres/100) /12 ) ) ** i;
+        capitalFinal = (eventCapitalInicial) * (1+((eventTasaDeInteres/100)/12))**i;
         resultadoInteres = capitalFinal.toFixed(2);
-        console.log("El capital final al mes " + i + " es: " + resultadoInteres + " $");
+        console.log(`»El capital final al mes ${i} es ${resultadoInteres} $`);
       }else{
-        capitalFinal = (capitalInicial + capitalReinversion * ( i-1 )) * ( 1 + ( (tasaDeInteres/100) /12 ) )**i;
+        capitalFinal = (eventCapitalInicial + eventCapitalReinversion*(i-1))*(1+((eventTasaDeInteres/100)/12))**i;
         resultadoInteres = capitalFinal.toFixed(2);
-        console.log("El capital final al mes " + i + " es: " + resultadoInteres + " $");
+        console.log(`»El capital final al mes ${i} es ${resultadoInteres} $`);
       }
-  }
-  let diferenciaCapital = capitalFinal - (capitalInicial + ( capitalReinversion * ( tiempoDeAhorro - 1 )));
-  let resultadoCapital = diferenciaCapital.toFixed(2);
-  console.log("La ganancia total en intereses, luego del tiempo de ahorro es de: " + resultadoCapital + " $");
+    }
+    let diferenciaCapital = capitalFinal - (eventCapitalInicial + ( eventCapitalReinversion * ( eventTiempoDeAhorro - 1 )));
+    let resultadoCapital = diferenciaCapital.toFixed(2);
+    console.log(`La ganancia total en intereses, luego del tiempo de ahorro es de: ${resultadoCapital} $`);
+    // let DOMCapitalFinal = document.createElement("div");
+    // DOMCapitalFinal.innerHTML = `
+    //                             <div class="row">
+    //                               <div class="col-lg-12">
+    //                                 <h3 class="finalCapitalText">C.F.</h3>
+    //                                 <h3 class="mount">${resultadoCapital} $</h3>
+    //                               </div>
+    //                             </div>`;
+    // document.body.appendChild(DOMCapitalFinal);
+    let DOMCapitalFinal = document.getElementById("DOMCapitalFinal_div");
+    let DOMCapitalFinal_h3 = document.createElement("h3");
+    DOMCapitalFinal_h3.innerHTML = `<p id="mountCapitalFinal_p">El interés ganado a los ${eventTiempoDeAhorro} meses es de: ${resultadoCapital} $</p><p id="mountCapitalFinal_p">El capital final es de: ${resultadoInteres} $</p>`;
+    DOMCapitalFinal.append(DOMCapitalFinal_h3);
+
   }
 }
 
@@ -59,12 +83,15 @@ let pedirProfesion = prompt("Ingrese su profesión:");
 databases.push(new ServicioInteres(pedirNombre, pedirApellido, pedirProfesion));
 for(const interes of databases){
   interes.mostrarUser();
-  interes.interesCompuesto();
 }
 
 //* Búsqueda del usuario segun las credenciales introducidas 
 //* por él mismo. 
 function isInDataBase(testDatabase, askProfession) {
+  /*
+   * Descripción: función que imprime un True por consola
+   * si la profesión del usuario es Economista.
+  */
   console.log("------------------"+"\n"+"[Testing] Validación de profesión."+"\n");
   testDatabase.some(function(CompProfession){
     // CompProfession se instancia con ServicioInteres. //
